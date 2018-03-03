3ad93dd20c85c59c5837fb519ab53615deb8f09b
MironovaE <mironova-ep@yandex.ru>
2018-03-03 21:07:57 +0300

    task done

--- work/main2.c
+++ work/main2.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include "task2.h"
-#define M 1000
 
 int main()
 {
-	char in[M] = { 0 };
-	char out[M] = { 0 };
+	char in[SIZE] = { 0 };
+	char out[SIZE] = { 0 };
 
 	printf("Please, enter a line:\n");
-	fgets(in, M, stdin);
+	fgets(in, SIZE, stdin);
 	if (in[strlen(in) - 1] == '\n')
 	{
 		in[strlen(in) - 1] = '\0';
